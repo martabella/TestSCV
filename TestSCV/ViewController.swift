bfe814fd3752015a67b6181fee953215c6593163
martabella <martabella@MacBook-Pro-de-Marta.local>
2019-03-18 17:05:53 +0100

    Add print in viewDidLoad

--- TestSCV/ViewController.swift
+++ TestSCV/ViewController.swift
@@ -1,18 +1,10 @@
-//
-//  ViewController.swift
-//  TestSCV
-//
-//  Created by Marta Bella on 18/03/2019.
-//  Copyright © 2019 CFGS La Salle Gracia. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello")
     }
 
 
