752de32062718a13e8444a8e875ed2689325e478
martabella <martabella@MacBook-Pro-de-Marta.local>
2019-03-18 17:03:09 +0100

    Initial Commit

---
+++ TestSCV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestSCV
+//
+//  Created by Marta Bella on 18/03/2019.
+//  Copyright © 2019 CFGS La Salle Gracia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
